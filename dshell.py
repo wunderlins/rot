57bcc830cb9577faa5163e62db42467f28daaa92
Simon Wunderlin <simon.wunderlin@usb.ch>
2015-01-08 14:16:01 +0100

    added

---
+++ dshell.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys, json
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+
+import config
+
+
+
+
